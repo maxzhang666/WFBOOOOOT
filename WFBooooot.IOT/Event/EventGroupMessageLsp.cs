684ea9fa4ab9425857007a34aee0c292e56e630a
Max zhang <373884384@qq.com>
2020-09-21 16:23:04 +0800

    增加一个LSP选项

--- WFBooooot.IOT/Event/EventGroupMessageLsp.cs
+++ WFBooooot.IOT/Event/EventGroupMessageLsp.cs
@@ -24,15 +24,21 @@ namespace WFBooooot.IOT.Event
                     if (key.Contains("来张色图"))
                     {
                         AppData.OpqApi.SendGroupMessage(e.FromGroup, "好嘞，马上就给你");
-                        AppData.OpqApi.SendMessage(new GroupImgMessage(e.FromGroup, "\r\n收好了您，哎~慢走", GetPicUrl()));
+                        var flag = e.FromQQ == 373884384 && (key.Contains("18") || key.Contains("牛批"));
+                        AppData.OpqApi.SendMessage(new GroupImgMessage(e.FromGroup, "\r\n收好了您，哎~慢走", GetPicUrl(flag)));
                     }
                 }
             }
         }
 
-        private string GetPicUrl()
+        /// <summary>
+        /// 获取图片地址
+        /// </summary>
+        /// <param name="isR18">是否获取R18</param>
+        /// <returns></returns>
+        private string GetPicUrl(bool isR18 = false)
         {
-            var res = GHttpHelper.Http.Get<Lsp>("https://api.lolicon.app/setu/?apikey=071046145f51a2a084d2c5");
+            var res = GHttpHelper.Http.Get<Lsp>($"https://api.lolicon.app/setu/?apikey=071046145f51a2a084d2c5&r18={(isR18 ? "1" : "0")}");
             return res.data.FirstOrDefault().url;
         }
     }
