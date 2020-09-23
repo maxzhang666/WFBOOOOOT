eead7653ef284f87d270920a04baf4739dca62ff
Max zhang <373884384@qq.com>
2020-09-23 13:55:46 +0800

    完善消息类型

--- OPQ.SDK/Enum/MessageType.cs
+++ OPQ.SDK/Enum/MessageType.cs
@@ -3,24 +3,58 @@ namespace OPQ.SDK.Enum
     public enum MessageType
     {
         /// <summary>
-        /// 文本消息
+        /// 纯文本消息[FG]，图文混搭时，会丢失图片成为TextMsg
         /// </summary>
         TextMsg,
+
         /// <summary>
-        /// 语音消息
+        /// 群成员AT消息[G]，图文混搭时，会成为PicMsg，AtMsg时，Content的子Type需要根据tips判断
         /// </summary>
-        VoiceMsg,
+        AtMsg,
+
         /// <summary>
-        /// 图片消息
+        /// 图片消息[FG]
         /// </summary>
         PicMsg,
+
         /// <summary>
-        /// 大表情
+        /// 大表情消息[FG]
         /// </summary>
         BigFaceMsg,
+
         /// <summary>
-        /// xml消息
+        /// 红包消息[F]，群红包为TextMsg
+        /// </summary>
+        RedBagMsg,
+
+        /// <summary>
+        /// 语音消息[FG]
+        /// </summary>
+        VoiceMsg,
+
+        /// <summary>
+        /// Json格式的复杂消息[FG]
+        /// </summary>
+        JsonMsg,
+
+        /// <summary>
+        /// Xml格式的复杂消息[FG]
         /// </summary>
         XmlMsg,
+
+        /// <summary>
+        /// 群文件消息[G]
+        /// </summary>
+        GroupFileMsg,
+
+        /// <summary>
+        /// 回复消息[FG]
+        /// </summary>
+        ReplayMsg,
+
+        /// <summary>
+        /// 其他类型的消息[FG]
+        /// </summary>
+        UnknownMsg
     }
 }
