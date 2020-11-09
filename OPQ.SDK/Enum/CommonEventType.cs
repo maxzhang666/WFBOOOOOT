dffe49c505a5bb0fde29f4c51a5062424178d516
Max zhang <373884384@qq.com>
2020-11-09 11:22:57 +0800

    更新通用事件类型

--- OPQ.SDK/Enum/CommonEventType.cs
+++ OPQ.SDK/Enum/CommonEventType.cs
@@ -8,13 +8,33 @@ namespace OPQ.SDK.Enum
         ON_EVENT_GROUP_JOIN,
 
         /// <summary>
+        /// 管理员变更
+        /// </summary>
+        ON_EVENT_GROUP_ADMIN,
+
+        /// <summary>
         /// 退群
         /// </summary>
         ON_EVENT_GROUP_EXIT,
 
         /// <summary>
+        /// 主动退群成功事件
+        /// </summary>
+        ON_EVENT_GROUP_EXIT_SUCC,
+
+        /// <summary>
         /// 踢出
         /// </summary>
-        ON_EVENT_GROUP_ADMINSYSNOTIFY
+        ON_EVENT_GROUP_ADMINSYSNOTIFY,
+
+        /// <summary>
+        /// 消息撤回
+        /// </summary>
+        ON_EVENT_GROUP_REVOKE,
+
+        /// <summary>
+        /// 群禁言
+        /// </summary>
+        ON_EVENT_GROUP_SHUT
     }
 }
