97684bf6664e3c03294b0da7a68114d9847c32fd
Max zhang <373884384@qq.com>
2020-11-10 10:10:58 +0800

    舔狗cd

--- WFBooooot.IOT/Event/EventGroupMessageDog.cs
+++ WFBooooot.IOT/Event/EventGroupMessageDog.cs
@@ -1,15 +1,26 @@
+using System;
 using WandhiBot.SDK.Event;
 using WandhiBot.SDK.EventArgs;
 using WFBooooot.IOT.Extension;
+using WFBooooot.IOT.Helper;
 using WFBooooot.IOT.Service;
 
 namespace WFBooooot.IOT.Event
 {
     public class EventGroupMessageDog : IGroupMessageEvent
     {
+        private CacheHelper _cacheHelper;
+        private const string Key = "dog";
+
+        public EventGroupMessageDog(CacheHelper cacheHelper)
+        {
+            _cacheHelper = cacheHelper;
+        }
+
         public void GroupMessage(GroupMessageEventArgs e)
         {
-            if (e.Msg.Text.Contains("舔狗"))
+            var last = _cacheHelper.Get<DateTime>(Key);
+            if (e.Msg.Text.Contains("舔狗") && (last == null || DateTime.Now - last > TimeSpan.FromMinutes(5)))
             {
                 var msg = new DogService(e.FromGroup).GetMsg();
                 if (msg.IsNotEmpty())
