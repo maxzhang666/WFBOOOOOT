051c4184d64fb9adc0669eb158a1c7d17f58fb5e
Max zhang <373884384@qq.com>
2021-03-26 17:16:49 +0800

    消息转发给主任

---
+++ WFBooooot.IOT/Event/FriendEvents/EventFriendMessageForward.cs
@@ -0,0 +1,19 @@
+using OPQ.SDK;
+using OPQ.SDK.Model.Friend;
+using WandhiBot.SDK.Event;
+using WandhiBot.SDK.EventArgs;
+
+namespace WFBooooot.IOT.Event.FriendEvents
+{
+    public class EventFriendMessageForward : IFriendMessageEvent
+    {
+        /// <summary>
+        /// 消息转发
+        /// </summary>
+        /// <param name="e"></param>
+        public void FriendMessage(FriendMessageEventArgs e)
+        {
+            AppData.OpqApi.SendMessage(new FriendMessage(373884384, e.Msg));
+        }
+    }
+}
