ed28bf1a11c41c661ed16f4d75c9856da841396d
Max zhang <373884384@qq.com>
2021-06-25 11:17:22 +0800

    淘宝链接附加签到红包参数

---
+++ WFBooooot.IOT/Event/GroupTaoLink.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text.RegularExpressions;
+using WandhiBot.SDK.Enum;
+using WandhiBot.SDK.Event;
+using WandhiBot.SDK.EventArgs;
+using WFBooooot.IOT.Extension;
+
+namespace WFBooooot.IOT.Event
+{
+    public class GroupTaoLink :IGroupMessageEvent
+    {
+        public void GroupMessage(GroupMessageEventArgs e)
+        {
+            if (e.FromGroup==937826612)
+            {
+                var reg = new Regex(@"https:\/\/m\.tb\.cn\/[a-zA-Z.0-9?=&]*",RegexOptions.IgnoreCase);
+                if (reg.IsMatch(e.Msg))
+                {
+                    var link = reg.Match(e.Msg);
+                    var linkStr = link?.Value;
+                    
+                    AppData.OpqApi.SendGroupMessage(e.FromGroup,$"来了老弟:{linkStr}&fpChannel=9 {e.FromQQ.AtUser()}");
+                }
+            }
+        }
+    }
+}
