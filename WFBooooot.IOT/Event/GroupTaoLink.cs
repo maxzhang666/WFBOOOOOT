a75bd838f9f61fd3b86a2a99fa5c10fe1ab9f4e0
Max zhang <373884384@qq.com>
2021-07-01 09:27:33 +0800

    淘宝消息忽略自身

--- WFBooooot.IOT/Event/GroupTaoLink.cs
+++ WFBooooot.IOT/Event/GroupTaoLink.cs
@@ -7,19 +7,19 @@ using WFBooooot.IOT.Extension;
 
 namespace WFBooooot.IOT.Event
 {
-    public class GroupTaoLink :IGroupMessageEvent
+    public class GroupTaoLink : IGroupMessageEvent
     {
         public void GroupMessage(GroupMessageEventArgs e)
         {
-            if (e.FromGroup==937826612||e.FromGroup==1019480370)
+            if (e.FromGroup == 937826612 || e.FromGroup == 1019480370 && e.FromQQ != AppData.OpqApi.CurrentQq)
             {
-                var reg = new Regex(@"https:\/\/m\.tb\.cn\/[a-zA-Z.0-9?=&]*",RegexOptions.IgnoreCase);
+                var reg = new Regex(@"https:\/\/m\.tb\.cn\/[a-zA-Z.0-9?=&]*", RegexOptions.IgnoreCase);
                 if (reg.IsMatch(e.Msg))
                 {
                     var link = reg.Match(e.Msg);
                     var linkStr = link?.Value;
-                    
-                    AppData.OpqApi.SendGroupMessage(e.FromGroup,$"来了老弟:{linkStr}&fpChannel=9 {e.FromQQ.AtUser()}");
+
+                    AppData.OpqApi.SendGroupMessage(e.FromGroup, $"来了老弟:{linkStr}&fpChannel=9 {e.FromQQ.AtUser()}");
                 }
             }
         }
